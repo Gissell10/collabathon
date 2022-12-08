faaaddbc05350b919e3f863da8f3505217d4be37
kylie“ <kyliexmq@gmail.com>
2022-12-09 09:40:04 +1100

    sub and foot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Icon } from '@iconify/react';
+import "../App.css";
 
 const Footer = () => {
     return (
-        <div>
-            <div className="footer-contact">
+        <div className="footer">
+            <div className="contact">
                 Contact us <br />
             
                 Subscribe
             </div> 
 
-            <div className="footer-about">
+            <div className="about">
                 About us
 
                 <div className="cup">
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Icon } from '@iconify/react';
 
-const Subscribe = () => {};
+const Subscribe = () => {
+
+    return (
+        <div>
+            <div className="subscribeword">
+                Do you want more juicy tea?<br />
+            
+                You can subscribe for more!
+            </div> 
+
+            <button className="subscrib">
+                Subscrib
+            </button>
+
+            <div className="cup">
+                <Icon icon="icon-park-outline:tea-drink" />
+            </div>
+
+            
+
+            
+
+        </div>
+    )
+};
 
 export default Subscribe;
