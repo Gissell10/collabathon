a303a66b412021931a41958df0ad2fef933d9fa5
kylie“ <kyliexmq@gmail.com>
2022-12-08 16:53:17 +1100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { Icon } from '@iconify/react';
 
-const Footer = () => {};
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer-contact">
+                Contact us <br />
+            
+                Subscribe
+            </div> 
+
+            <div className="footer-about">
+                About us
+
+                <div className="cup">
+                    <Icon icon="icon-park-outline:tea-drink" />
+                </div>
+
+            </div>
+
+            
+
+        </div>
+
+        
+
+
+
+    )
+};
 
 export default Footer;
