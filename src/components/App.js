2d59d4c983cff7eec0701e7103add8419ea88124
Gissell Bustos <gissellbustosb@gmail.com>
2022-12-08 14:09:07 +1100

    components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Home></Home>
+      <Singin></Singin>
+    </div>
+  );
+}
+
+export default App;
