20da674a04f9f9a2ee055d5b74a4d4236a098027
Gissell Bustos <gissellbustosb@gmail.com>
2022-12-08 21:30:43 +1100

    Nav styles

---
+++ src/components/Article.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import image from "../image/image.png";
+
+const Article = () => {
+  return (
+    <div>
+      <img src={image} alt="Image" height={187} width={184} />
+    </div>
+  );
+};
+
+export default Article;
