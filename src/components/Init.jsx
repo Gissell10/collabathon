6ce1429fd36d19585bd871a40252b99077f9535f
Gissell Bustos <gissellbustosb@gmail.com>
2022-12-08 17:17:02 +1100

    init

--- src/components/Init.jsx
+++ src/components/Init.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Init = () => {};
+const Init = () => {
+  return (
+    <div>
+      <div className="bar">
+        <p>Celebrity News</p>
+        <p>Toilet</p>
+        <div className="white-bar"></div>
+        <p>Recipes</p>
+      </div>
+      <div></div>
+      <button>Juicy Hell</button>
+    </div>
+  );
+};
 
 export default Init;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const Nav = () => {
   return (
     <div className="Nav">
       <div>
-        <img src="public/hamburger-menu.png" alt="" />
+        <Icon icon="icon-park:hamburger-button" />
       </div>
       <h1>
         GIVE ME <br /> THE TEA
       </h1>
       <button>SUSCRIBE</button>
-      <div className="user"> </div>
+      <div>
+        <Icon icon="mdi:user-circle-outline" />
+      </div>
     </div>
   );
 };
