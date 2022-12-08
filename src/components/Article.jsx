546d5de081600c9c041990d436c54c19a7cb347d
Gissell Bustos <gissellbustosb@gmail.com>
2022-12-09 10:15:39 +1100

    articles

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
-const Article = ({
-  children,
-  image,
-  title,
-  description = "The crazy fox jumped over the old dog",
-}) => {
+const Article = ({ children, image, title, description, time, callAction }) => {
   return (
     <div>
+      {callAction !== undefined ? (
+        <button>
+          <span>
+            <Icon icon="gridicons:cross-circle" color="#0026ad" />
+          </span>
+          {callAction}
+        </button>
+      ) : (
+        <p></p>
+      )}
+
+      <span> {time}</span>
       <h2>{title}</h2>
       <img src={image} alt="Image" height={187} width={184} />
-      <p>{description}</p>
+      <p className="description">{description}</p>
     </div>
   );
 };
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -10,10 +10,32 @@ const Articles = () => {
         <button className="Articles-button">Juicy Juicy Hell Today</button>
       </div>
       <div></div>
-      <Article image={toiletOne} title="Lorem Ipsum"></Article>
-      <Article image={toiletTwo} title="Lorem Ipsum"></Article>
-      <Article image={toiletTwo} title="Lorem pepito"></Article>
-      <Article image={toiletOne} title="Lorem Ipsum"></Article>
+      <Article
+        callAction="Hot"
+        time="10 mins ago!"
+        image={toiletOne}
+        title="HOW LADY GAGA CLEANS HER TOILET"
+        description="The Australian actress has made a rare announcement about her private life, as photos of her husband were published online today."
+      ></Article>
+      <Article
+        callAction="Must have"
+        time="1 hour ago "
+        image={toiletTwo}
+        title="HOW RIHANNA WIPES HER BUM"
+        description="The Australian actress has made a rare announcement about her private life, as photos of her husband were published online today."
+      ></Article>
+      <Article
+        time="5 hours ago "
+        image={toiletTwo}
+        title="KANYE REVEALS HIS LOO  SECRET "
+        description="The Australian actress has made a rare announcement about her private life, as photos of her husband were published online today."
+      ></Article>
+      <Article
+        time="10 hours ago "
+        image={toiletOne}
+        title="HOW KYLIE JENNER CHOPS A LOG"
+        description="The Australian actress has made a rare announcement about her private life, as photos of her husband were published online today."
+      ></Article>
     </div>
   );
 };
